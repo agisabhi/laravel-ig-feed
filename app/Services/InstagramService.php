51fbb9f68912ac1f41648baf6af94ddbdf749595
agisabhi <119267406+agisabhi@users.noreply.github.com>
2024-07-23 10:41:18 +0700

    laravel ig feed

---
+++ app/Services/InstagramService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+
+class InstagramService
+{
+    protected $client;
+    protected $accessToken;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->accessToken = env('INSTAGRAM_ACCESS_TOKEN');
+    }
+
+    public function getUserMedia()
+    {
+        try {
+            $response = $this->client->request(
+                'GET',
+                'https://graph.instagram.com/me/media',
+                [
+                    'query' => [
+                        'fields' => 'id,caption,media_type,media_url,thumbnail_url,permalink',
+                        'access_token' => $this->accessToken,
+                    ],
+                ]
+            );
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (\Exception $e) {
+            return ['data' => []];
+        }
+    }
+}
